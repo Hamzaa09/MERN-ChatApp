a75dbb6b52883cbcb80559dca88fb9686ef10c54
Hamzaa09 <hamza036513@gmail.com>
2025-10-05 19:43:41 +0500

    test

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,9 +6,9 @@ const router = express()
 
 router.post('/login', login)
 router.post('/signup', register)
-router.post('/logout', authCheck, logout)
-router.get('/getProfile', authCheck, getProfile)
-router.get('/getOthers', authCheck, otherUsers)
-router.patch('/profileupdate', authCheck, userUpdate)
+router.post('/logout', logout)
+router.get('/getProfile', getProfile)
+router.get('/getOthers', otherUsers)
+router.patch('/profileupdate', userUpdate)
 
 export default router
